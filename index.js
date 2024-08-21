3c51205451f89a97da51b41e3b50576666d13c02
prarthana-v <p.d.vaghani1729@gmail.com>
2024-08-21 08:18:02 +0530

    html to ejs

--- index.js
+++ index.js
@@ -1,18 +1,82 @@
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
-const app = express();
 const port = 8000;
-
-app.use(expressLayouts);
+const app = express();
 app.set("view engine", "ejs");
+const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.set("/", (req, res) => {});
+app.get("/", (req, res) => {
+  return res.render("dashboard");
+});
+
+app.get("/charts", (req, res) => {
+  return res.render("charts");
+});
+
+app.get("/widgets", (req, res) => {
+  return res.render("widgets");
+});
+
+app.get("/tables", (req, res) => {
+  return res.render("tables");
+});
+
+app.get("/grid", (req, res) => {
+  return res.render("grid");
+});
+
+app.get("/form-basic", (req, res) => {
+  return res.render("form-basic");
+});
+
+app.get("/form-wizard", (req, res) => {
+  return res.render("form-wizard");
+});
+
+app.get("/page-buttons", (req, res) => {
+  return res.render("page-buttons");
+});
+
+app.get("/icon-material", (req, res) => {
+  return res.render("icon-material");
+});
+
+app.get("/icon-fontawesome", (req, res) => {
+  return res.render("icon-fontawesome");
+});
+
+app.get("/page-element", (req, res) => {
+  return res.render("page-element");
+});
+
+app.get("/index2", (req, res) => {
+  return res.render("index2");
+});
+
+app.get("/page-gallary", (req, res) => {
+  return res.render("page-gallary");
+});
+
+app.get("/page-calender", (req, res) => {
+  return res.render("page-calender");
+});
+
+app.get("/pages-invoice", (req, res) => {
+  return res.render("pages-invoice");
+});
+
+app.get("/page-chat", (req, res) => {
+  return res.render("page-chat");
+});
+
+app.get("/authentication-login", (req, res) => {
+  return res.render(" authentication-login");
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
     return false;
   }
-  console.log(`server is start on port :- ${port}`);
+  console.log(`Server Run in port ${port}`);
 });
